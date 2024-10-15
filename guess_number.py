d496fcb5d3852117462ee32888f5f9361ff22daa
ZakkFromSource <zakkfromsource@gmail.com>
2024-10-16 00:04:03 +0100

    Added to do list.

--- guess_number.py
+++ guess_number.py
@@ -1,27 +1,17 @@
 import sys
 import random
 
+"""TO DO
 
-#     def guess_num_game():
-#         print(greeting)
+Add Player name entry and personalize the game.
 
-# if __name__ == "__main__":
-#     import argparse
+Make Qutting guess_number.py and rps.py return to arcade.py menu.
 
-#     # Create parser using argparse module
-#     parser = argparse.ArgumentParser(
-#         description="Creates a personalised game experience."
-#     )
+Make Press 'X' to exit game from arcade menu.
 
-#     # Add arguments to parser
-#     parser.add_argument("name", help="The name of the player.")
-
-#     # Parse the arguments
-#     args = parser.parse_args()
+"""
 
 
-#     greeting = f"Hello {args.name}!"
-#     guess_num_game()
 def guess_no_game():
     gamecount = 0
     player_wins = 0
@@ -32,7 +22,11 @@ def guess_no_game():
         try:
             player_choice = input("\nPlease pick the number 1, 2 or 3...\n\n")
 
-            if player_choice not in ["1", "2", "3"]:
+            if player_choice not in [
+                "1",
+                "2",
+                "3",
+            ]:  # Ensures player can only pick between 1-3
                 print("\nTry again...")
                 return play_guess_num()
             else:
@@ -41,13 +35,13 @@ def guess_no_game():
         except ValueError:
             sys.exit("\nInvalid input. Please enter a valid number next time.\n")
 
-        python_int = random.randint(1, 3)  # Python's number
+        python_int = random.randint(1, 3)  # Python's number between 1-3
 
         print(f"\nPlayer chose {player_int}.")
 
         print(f"I was thinking of the number {python_int}.\n")
 
-        def win_rate():
+        def win_rate():  # Player Win Percentage calculator
             nonlocal player_wins
             nonlocal python_wins
             win_percentage = player_wins / (player_wins + python_wins) * 100
@@ -94,3 +88,25 @@ def guess_no_game():
 
 
 guess_no_game()
+
+
+#     def guess_num_game():
+#         print(greeting)
+
+# if __name__ == "__main__":
+#     import argparse
+
+#     # Create parser using argparse module
+#     parser = argparse.ArgumentParser(
+#         description="Creates a personalised game experience."
+#     )
+
+#     # Add arguments to parser
+#     parser.add_argument("name", help="The name of the player.")
+
+#     # Parse the arguments
+#     args = parser.parse_args()
+
+
+#     greeting = f"Hello {args.name}!"
+#     guess_num_game()
